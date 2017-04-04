7f947e13714bd91c37ede44b7fd75e4de5b8afaa
github username <github email address>
2017-04-04 18:22:06 -0400

    added 'did you mean'

--- lib/test.rb
+++ lib/test.rb
@@ -11,16 +11,30 @@ end
 
 def get_character_movies_from_api(searched_character_hash)
   films = []
-  films_array = searched_character_hash["results"].first["films"]
-  films_array.each {|i| films << JSON.parse(RestClient.get(i))}
-  films
+  if searched_character_hash["count"] > 1
+    puts "Did you mean:"
+    searched_character_hash["results"].each {|x| puts x["name"]} 
+    puts "*" * 30
+    recheck
+    films
+  else
+    films_array = searched_character_hash["results"].first["films"]
+    films_array.each {|i| films << JSON.parse(RestClient.get(i))}
+    films
+  end
+end
+
+def recheck
+  character = get_character_from_user
+  character_info = fetch_data("http://www.swapi.co/api/people/?search=#{character}")
+  show_character_movies(character_info)
 end
 
 def parse_character_movies(films_hash)
   films_hash.each {|movie| puts movie["title"]}
 end
 
-def show_character_movies(searched_character_hash)
-  films_hash = get_character_movies_from_api(searched_character_hash)
+def show_character_movies(character)
+  films_hash = get_character_movies_from_api(character)
   parse_character_movies(films_hash)
 end
